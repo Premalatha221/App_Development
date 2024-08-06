ad4f0e425e1d76b608f77189e933a70dab06f008
Premalatha221 <152426858+Premalatha221@users.noreply.github.com>
2024-08-06 09:41:04 +0530

    Add files via upload

---
+++ `Day 9/config/UsersDetailsService.java
@@ -0,0 +1,29 @@
+package com.backend.toystore.config;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.backend.toystore.model.User;
+import com.backend.toystore.repository.UserRepo;
+
+
+
+@Component
+public class UsersDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userInfo = repository.findByEmail(username);
+        return userInfo.map(UsersDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found " + username));
+
+    }
+}
